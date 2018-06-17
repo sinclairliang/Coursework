09c4e43f71e25db026cbbe1bac3515edfc2d836d
Sinclair Liang <sinclairliang@gmail.com>
2018-06-17 15:55:06 -0700

    adding anagram programme in Java

---
+++ CMPS101/CMPS101S18PA3/FindAnagram.java
@@ -0,0 +1,106 @@
+/* ******************************************************/
+// CREATED: Sinclair Liang on May 9th, 2018;
+// CHANGED: Sinclair Liang on May 9th, 2018: Sorting by code;
+// CHANGED: Sinclair Liang on May 10th, 2018: Finished I/O part;
+// CHANGED: Sinclair Liang on May 12th, 2018: Adding Comments;
+/* ******************************************************/
+
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Collections;
+
+public class FindAnagram extends anagram {
+	public static void main(String[] args) throws IOException {
+		int lineNumber = 0;
+		Scanner wordlist = new Scanner(new File(args[0]));
+
+		while ( wordlist.hasNextLine() ) {
+			// processing wordlist dictionary file;
+			lineNumber++;
+			// calculating how many lines it has;
+			wordlist.next();
+		}
+
+		wordlist.close();
+		wordlist = new Scanner(new File(args[0]));
+		String[] pre_gram = new String[lineNumber];
+		// creat a giant array with the number of lines of slots;
+
+		for (int i = 0; i < lineNumber; i++) {
+			// store each line/word into the giant array;
+			pre_gram[i] = wordlist.next();;
+		}
+
+		anagram[] grams = new anagram[lineNumber];
+		// creat another array to store anagram type;
+
+		for (int i = 0; i < lineNumber; i++) {
+			// store each anagram of those words into the array;
+			grams[i] = new anagram(pre_gram[i]);
+		}
+
+		Arrays.sort(grams);
+		// sort the giant array of anagrams;
+		ArrayList<anagram> arrli = new ArrayList<anagram>(lineNumber);
+		// create an arraylist;
+
+		for (int i = 0; i < lineNumber; i++) {
+			// covert the giant array of anagrams to an arraylist;
+			arrli.add(new anagram(pre_gram[i]));
+		}
+
+		Collections.sort(arrli);
+		// sort the arraylist;
+
+		while(true)
+		{
+			int anagram_found = 0;
+			Scanner reader = new Scanner(System.in);
+			System.out.println("type a string of letters");
+			String s_test = reader.next();
+			// read in user input;
+			anagram test = new anagram(s_test);
+
+			int index = Collections.binarySearch(arrli, test);
+			// using Arraylist search to find rhe matching anagram;
+			if (index > 0) 
+			{
+				// if found;
+				index = index - 10;
+				// Assuming there is no more than 10 anagrams for any given word;
+				for (int i = 0; i < 20 ; i++) 
+				{
+					if (test.code == arrli.get(index + i).code) 
+					{
+						if (!test.field_string.equals(arrli.get(index + i).field_string)) 
+						{
+							System.out.println(arrli.get(index + i).field_string);
+							anagram_found++;
+						}
+					}
+				}
+				if (anagram_found<1)
+				{
+					// when no anagram is found;
+					System.out.println("No other Found.");		
+				}
+			}
+			else
+			{
+				// when no anagram is found;
+				System.out.println("Not Found.");
+			}
+
+			Scanner user_choice = new Scanner(System.in);
+			System.out.println("Do another (y/n)?");
+			String user_answer = user_choice.next();
+			if(user_answer.equals("n"))
+			{
+				break;
+			}		
+		}
+	}
+}
---
+++ CMPS101/CMPS101S18PA3/anagram.java
@@ -0,0 +1,124 @@
+/* ******************************************************/
+// CREATED: Sinclair Liang on May 7th, 2018;
+// CHANGED: Sinclair Liang on May 12th, 2018: Adding Comments;
+/* ******************************************************/ 
+
+import java.lang.*;
+import java.util.Arrays;
+import java.lang.Math;
+
+
+class anagram implements Comparable<anagram>
+{
+	public String field_string;
+	public int code;
+
+
+//--------------------------------------------------------
+// Constructors
+//--------------------------------------------------------
+
+
+	public anagram()
+	{
+	    // default constructor;
+		field_string = "";
+		code = 0;
+	}
+
+
+	public anagram(String s)
+	{
+		// constructor for string type;
+		String s1 = s.toLowerCase();
+		// convert all letters to lowercased;
+		char tempArray[] = s1.toCharArray();
+		// initiate an empty char array for sorting later;
+		Arrays.sort(tempArray);
+		// sort
+		String s2 = new String(tempArray);
+		field_string = s1;
+		code = s2.hashCode();
+		// take the hashcode for that sorted lowercased string to be the code;
+	}
+
+	public anagram(char[] array)
+	{
+		// parameter: a char array;
+		// returns: the anagram type of it;
+		field_string = String.valueOf(array);
+		String s1 = field_string.toLowerCase();
+		char tempArray[] = s1.toCharArray();
+		Arrays.sort(tempArray);
+		String s2 = new String(tempArray);
+		code = s2.hashCode();
+		// code = an.code;
+	}
+
+//--------------------------------------------------------
+// methods
+//--------------------------------------------------------
+	public static void print(anagram z)
+	{
+		// parameter: anagram type;
+		// returns: null;
+		// what it does: print the string field of that anagram;
+		System.out.print(z.field_string);
+	}
+
+	boolean compare(anagram B)
+	{
+		// parameter: anagram type;
+		// returns: a boolean value to indicate wether it is the anagram with that word;
+		if (this.code == B.code) 
+		{
+			// comparing by the codes;
+			return true;
+		}
+		return false;
+	}
+
+	String word_part(anagram A)
+	{
+		// parameter: anagram type;
+		// returns: the string field of that anagram;
+		return A.field_string;
+	}
+
+	public static int hashing(String s)
+	{
+		// another method for generating codes I thought of;
+		String s1 = s.toLowerCase();
+		int code = 0;
+		for (int i = 0; i < s1.length(); i++)
+		{
+			char c = s1.charAt(i);
+			code += (int)c;
+		}
+		return code;
+	}
+
+    @Override
+    public int compareTo(anagram comparesto) 
+    {
+    	// overwting the compareTo method for comparsion;
+    	// comparing by the codes;
+        if (this.code < comparesto.code) 
+        {
+        	return -1;
+        }
+        else if (this.code > comparesto.code) 
+        {
+        	return 1;
+        }
+        else
+        {
+        	return 0;
+        }
+	}
+
+}
+
+
+
+
