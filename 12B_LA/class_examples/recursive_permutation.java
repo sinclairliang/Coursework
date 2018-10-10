9546af33f0f228b04c64cca02ce1c3b47723858c
Sinclair Liang <sinclairliang@gmail.com>
2018-10-10 01:40:28 -0700

    working on 09/10/2018 notes

---
+++ 12B_LA/class_examples/recursive_permutation.java
@@ -0,0 +1,27 @@
+class recursive_permutation
+{
+	public static void main(String[] args)
+	{
+		int[] A = {1,2,3};
+
+	}
+
+	public static void printAllsub_front(int[] A, int i, int[] B)
+	{
+
+	}
+
+
+
+
+
+	static void printArray(int[] A)
+	{
+		for (int i=0;i<A.length;i++) 
+		{
+			System.out.print(A[i]);
+		}
+		System.out.println();
+	}
+
+}
