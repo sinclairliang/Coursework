a1e92755e6a0d810750bec1aa790bd6169a406f2
Sinclair Liang <sinclairliang@gmail.com>
2018-10-17 02:40:14 -0700

    updated README

--- 12B_LA/README.md
+++ 12B_LA/README.md
@@ -21,10 +21,11 @@ Sinclair's Session:
 |Date   |Topic   |Notes   |Reference   |Key Words   |
 |---|---|---|---|---|
 |27/09/2018   |First Class   |[Notes](https://github.com/sinclairliang/Coursework/blob/master/12B_LA/Notes/Notes_27_09_2018.md)   |   |Backtracking, Recursion   |
-|02/10/2018   |Class   |[Notes](https://github.com/sinclairliang/Coursework/blob/master/12B_LA/Notes/Notes_02_10_2018.md)   |   |Box Trace, Recursion   |
+|02/10/2018   |Class     |[Notes](https://github.com/sinclairliang/Coursework/blob/master/12B_LA/Notes/Notes_02_10_2018.md)   |   |Box Trace, Recursion   |
 |04/10/2018   |Class     |   |   |Intro to NQueens Problem|
 |09/10/2018   |Class     |[Notes](https://github.com/sinclairliang/Coursework/blob/master/12B_LA/Notes/Notes_09_10_2018.md)||Abstraction, ADT|
 |11/10/2018   |Class     |[Notes](https://github.com/sinclairliang/Coursework/blob/master/12B_LA/Notes/Notes_11_10_2018.md)||LinkedList, Reference Type in Java|
+|16/10/2018   |Class     |[Notes](#)||Pointers in C|
 |23/10/2018   |Midterm 1 |   |   |   |
 |29/11/2018   |Midterm 2 |   |   |   |
 |14/12/2018   |Last Day of Instruction|   || No Finals!  |
--- CMPS102/Algorithms_in_Class/stabel_matching_algorithm.py
+++ CMPS102/Algorithms_in_Class/stabel_matching_algorithm.py
@@ -3,61 +3,81 @@ Tribute to https://www.youtube.com/watch?v=FhRf0j068ZA
 and Pride and Prejudice by Jane Austen
 """
 
+# preferred_rankings_men = {
+# 	'Binley':  ['Jane', 'Elizabeth', 'Lydia', 'Charlotte'],
+# 	'Wickham': ['Lydia', 'Jane', 'Elizabeth', 'Charlotte'],
+# 	'Darcy':   ['Elizabeth', 'Jane', 'Charlotte', "Lydia"],
+# 	'Collins': ['Jane', 'Elizabeth', 'Lydia', 'Charlotte']
+# }
+
+# preferred_rankings_women = {
+# 	'Jane' :     ['Binley', 'Wickham', 'Darcy', 'Collins'],
+# 	'Lydia':     ['Binley', 'Wickham', 'Darcy', 'Collins'],
+# 	'Elizabeth': ['Wickham', 'Darcy', 'Binley', 'Collins'],
+# 	'Charlotte': ['Binley', 'Darcy', 'Collins', 'Wickham']
+# }
+
+
 preferred_rankings_men = {
-	'Binley':  ['Jane', 'Elizabeth', 'Lydia', 'Charlotte'],
-	'Wickham': ['Lydia', 'Jane', 'Elizabeth', 'Charlotte'],
-	'Darcy':   ['Elizabeth', 'Jane', 'Charlotte', "Lydia"],
-	'Collins': ['Jane', 'Elizabeth', 'Lydia', 'Charlotte']
+	'Ben':    ['Alice', 'Sarah', 'Betty'],
+	'Carl':   ['Alice', 'Betty', 'Sarah'],
+	'David':  ['Sarah', 'Alice', 'Betty']
 }
 
 preferred_rankings_women = {
-	'Jane' :     ['Binley', 'Wickham', 'Darcy', 'Collins'],
-	'Lydia':     ['Binley', 'Wickham', 'Darcy', 'Collins'],
-	'Elizabeth': ['Wickham', 'Darcy', 'Binley', 'Collins'],
-	'Charlotte': ['Binley', 'Darcy', 'Collins', 'Wickham']
+	'Alice' : ['David', 'Carl', 'Ben'],
+	'Sarah':  ['Ben', 'David', 'Carl'],
+	'Betty':  ['Carl', 'David', 'Ben']
 }
+
+# preferred_rankings_women = {
+# 	# truethful
+# 	'Alice' : ['David', 'Ben', 'Carl'],
+# 	'Sarah':  ['Ben', 'David', 'Carl'],
+# 	'Betty':  ['Carl', 'David', 'Ben']
+# }
 
 tentative_engagements = []
 
-free_women = []
+free_men = []
 
-def init_free_women():
-	for woman in preferred_rankings_women.keys():
-		free_women.append(woman)
+def init_free_men():
+	for man in preferred_rankings_men.keys():
+		free_men.append(man)
 
 
 def stable_matching():
-	while (len(free_women) > 0):
-		for woman in free_women:
-			begin_matching(woman)
+	while (len(free_men) > 0):
+		for man in free_men:
+			begin_matching(man)
 
-def begin_matching(woman):
-	print("Matching for this woman: {}".format(woman))
-	for man in preferred_rankings_women[woman]:
-		taken_match = [couple for couple in tentative_engagements if man in couple]
+def begin_matching(man):
+	print("Matching for this man: {}".format(man))
+	for woman in preferred_rankings_men[man]:
+		taken_match = [couple for couple in tentative_engagements if woman in couple]
 
 		if (len(taken_match) == 0):
-			tentative_engagements.append([woman, man])
+			tentative_engagements.append([man, woman])
 
-			free_women.remove(woman)
-			print("{} is no longer a free woman and now tentatively engaged to {}".format(woman, man))
+			free_men.remove(man)
+			print("{} is no longer a free man and now tentatively engaged to {}".format(man, woman))
 			break
 
 		elif (len(taken_match) > 0):
-			print("Person {} is taken already...".format(man))
-			current_girl_ranking = preferred_rankings_men[man].index(taken_match[0][0])
-			potential_girl_ranking = preferred_rankings_men[man].index(woman)
+			print("Person {} is taken already...".format(woman))
+			current_girl_ranking = preferred_rankings_women[woman].index(taken_match[0][0])
+			potential_girl_ranking = preferred_rankings_women[woman].index(man)
 
 			if (current_girl_ranking < potential_girl_ranking):
-				print("He ({}) is satisfied with {}...".format(taken_match[0][1],taken_match[0][0]))
+				print("She ({}) is satisfied with {}...".format(taken_match[0][1],taken_match[0][0]))
 			else:
 				print("{} is better than the old girl {}".format(woman, taken_match[0][0]))
 				print("{} is free agian".format(taken_match[0][0]))
 
-				free_women.remove(woman)
-				free_women.append(taken_match[0][0])
+				free_men.remove(man)
+				free_men.append(taken_match[0][0])
 
-				taken_match[0][0] = woman
+				taken_match[0][0] = man
 				break
 	print("\n")	
  
@@ -67,13 +87,13 @@ def begin_matching(woman):
 
 def main():
 	print("Hello World!")
-	init_free_women()
+	init_free_men()
 	stable_matching()
 
 	print("\t----COMPLETE----\t")
 	print("\n")	
 	for couple in tentative_engagements:
-		print("{} is engaged to {}".format(couple[1], couple[0]))
+		print("{} is engaged to {}".format(couple[0], couple[1]))
 
 if __name__ == '__main__':
 	main()
