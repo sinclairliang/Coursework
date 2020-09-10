fcb26ea81bb140c7c6d9cced4755bb71bc9788af
Sinclair Liang <sinclairliang@gmail.com>
2020-09-09 21:52:46 -0500

    I graduated

--- 5P/asgn1/assignment1.py
+++ 5P/asgn1/assignment1.py
@@ -8,26 +8,26 @@
 
 
 import math
+
 length = input("What is the straightaway length (in yards)?\n")
 radius = input("What is the turn radius (in yards)?\n")
 width = input("What is the track width (in feet)?\n")
 depth = input("What is the track depth (in inches)?\n")
 pi = math.pi
 
-#unit conversion: http://www.metric-conversions.org/length/yards-to-meters.htm
-length_meter = 2*float(length)*0.91440
-radius_meter = float(radius)*0.91440
-#unit conversion: http://www.metric-conversions.org/length/feet-to-meters.htm
-width_meter = float(width)*0.3048
-#unit conversion: http://www.rapidtables.com/convert/length/inch-to-meter.htm
-depth_meter = float(depth)*0.0254
+# unit conversion: http://www.metric-conversions.org/length/yards-to-meters.htm
+length_meter = 2 * float(length) * 0.91440
+radius_meter = float(radius) * 0.91440
+# unit conversion: http://www.metric-conversions.org/length/feet-to-meters.htm
+width_meter = float(width) * 0.3048
+# unit conversion: http://www.rapidtables.com/convert/length/inch-to-meter.htm
+depth_meter = float(depth) * 0.0254
 
+# Calculate the area of two straight ways
+s0 = length_meter * width_meter
+# Calculate the area of the ring, which is formed by two circles
+s1 = (pi * ((radius_meter + width_meter) ** 2) - (pi * ((radius_meter) ** 2)))
+# Calculate the Volume
+volume = (s0 + s1) * depth_meter
 
-#Calculate the area of two straight ways
-s0 = length_meter*width_meter
-#Calculate the area of the ring, which is formed by two circles
-s1 = (pi*((radius_meter+width_meter)**2)-(pi*((radius_meter)**2)))
-#Calculate the Volume
-volume = (s0+s1)*depth_meter
-
-print("Total volume of dirt (in cubic meters) is "+str(volume))
+print("Total volume of dirt (in cubic meters) is " + str(volume))
